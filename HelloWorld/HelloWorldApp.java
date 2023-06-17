3ece0cb619428f6e85f5fe14a6dd4b9790115d86
YujinJeon888888 <117812553+YujinJeon888888@users.noreply.github.com>
2023-06-17 16:44:54 +0900

    helloWorld

---
+++ HelloWorld/HelloWorldApp.java
@@ -0,0 +1,15 @@
+public class HelloWorldApp{
+
+    //2. 메인메소드를 만들고
+
+     public static void main(String [] args){
+
+               //3. 화면에 출력할 내용을 " "안에 적은 후  실행하고 
+
+                System.out.println("안녕하세요!");
+
+               System.out.println("객체지향프로그래밍을 시작합니다.");
+
+     }
+
+}           //4. 실행된 화면은 캡춰하여 게시판에 올리면 됩니다.
